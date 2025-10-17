e2971fbcdf8c51a3d106f8721d4a1c87bbf51697
pasanpiyumantha98 <139437240+pasanpiyumantha98@users.noreply.github.com>
2025-10-17 09:05:35 +0530

    Update userRoute.js

--- Back-RankReminder/Routes/userRoute.js
+++ Back-RankReminder/Routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import getDb from '../db.js';
+import bycrypt from 'bcrypt';
 
 const db = await getDb();
 
@@ -40,13 +41,49 @@ router.post('/insert', (req, res) => {
             return;
         }
 
-        await db.collection("Users").insertOne({id:id, username:username, email:email, tier:"Free", password:password});
+        const hasshedPass = await bycrypt.hash(password, 10);
+
+        await db.collection("Users").insertOne({id:id, username:username, email:email, tier:"Free", password:hasshedPass});
 
         res.send('success');
 
     }
 
     insertUser();
+
+
+})
+
+
+router.post('/login', (req,res)=>{
+
+const email = req.body.email;
+const password = req.body.password;
+
+
+async function loginUser() {
+
+const user = await db.collection("Users").findOne({email: email});
+
+if(!user)
+{
+    return res.send('NoUser');
+}
+
+const passMatch = await bycrypt.compare(password, user.password);
+
+if(!passMatch)
+{
+    return res.send('WrongPass');
+}
+else{
+    return res.send({status:'success', username:user.username, tier:user.tier});    
+}
+
+}
+
+loginUser();
+
 
 
 })
